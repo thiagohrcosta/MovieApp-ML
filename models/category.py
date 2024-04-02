85f55b3579f41bdb4a0660990a0b1b1967a732a8
ThiagoHRCosta <thiagohrcosta86@gmail.com>
2024-04-01 21:16:37 -0300

    fix database and show categories

--- models/category.py
+++ models/category.py
@@ -5,7 +5,6 @@ class Category(db.Model):
   id = db.Column(db.Integer, unique=True, primary_key=True)
   category_external_id = db.Column(db.Integer, unique=True)
   title = db.Column(db.String(32), nullable=False)
-  movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'), nullable=True)
 
   def serialize(self):
 

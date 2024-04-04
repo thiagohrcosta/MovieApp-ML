09d8d9db3c7a29fc2beb39bc93907aed7c321753
ThiagoHRCosta <thiagohrcosta86@gmail.com>
2024-04-04 17:26:29 -0300

    Fetch movie list and add favorites

--- app.py
+++ app.py
@@ -94,7 +94,8 @@ def list_categories():
 
 @app.route('/fetch_movies', methods=['POST'])
 def fetch_movies():
-  for n in range(1, 3):  
+  for n in range(1, 150):  
+    print(n)
     api_url = f"https://api.themoviedb.org/3/movie/popular?language=en-US&page={n}&api_key={movie_database_api_key}"
     response = requests.get(api_url)
     data = response.json()
@@ -121,22 +122,96 @@ def fetch_movies():
 
       existing_movie = Movie.query.filter_by(title=movie['title']).first()
 
-      if existing_movie:
-        continue
+      if not existing_movie:
+        # continue
+        db.session.add(movie_data)
+        db.session.commit()
 
-      db.session.add(movie_data)
-      db.session.commit()
-
-    return jsonify({'message': 'Movies fetched and stored successfully'})
+  movies_count = Movie.query.count()
+  return jsonify({'message': 'Movies fetched and stored successfully', 'total': movies_count})
   
-
-      # writer.writerow([title, genre_ids, adult, original_language, release_date, overview, vote_average, popularity, poster])
-
 @app.route('/movies', methods=['GET'])
 def list_movies():
   movies = Movie.query.all()
   serialized_movies = [movie.serialize() for movie in movies]
   return jsonify(serialized_movies)
+
+@app.route('/favorite/user/<int:user_id>', methods=['GET'])
+def list_user_favorite(user_id):
+  user_favorites = Favorite.query.filter_by(user_id=user_id).all()
+
+  favorite_titles = []
+
+  for favorite in user_favorites:
+      movie = Movie.query.get(favorite.movie_id)
+      favorite_titles.append(movie.title)
+
+  return jsonify({'favorites': favorite_titles})
+
+@app.route('/favorite', methods=['POST'])
+def add_favorite():
+  data = request.json
+  user_id = data.get('user_id')
+  movie_title = data.get('movie_title')
+
+  movie = Movie.query.filter_by(title=movie_title).first()
+
+  user = User.query.filter_by(id=user_id).first()
+
+  if not user:
+    return jsonify({'message', "User not found"}), 400
+
+  if movie:
+    movie_id = movie.id
+  else:
+    api_url = f"https://api.themoviedb.org/3/search/movie?query={movie_title}&api_key={movie_database_api_key}"
+    response = requests.get(api_url)
+    data = response.json()
+
+    for movie in data['results']:
+      genre_ids = movie['genre_ids']
+
+      categories = Category.query.filter(Category.category_external_id.in_(genre_ids)).all()
+
+      movie_categories = [category for category in categories]
+
+      movie_data = Movie(
+        title=movie['title'],
+        # genre_ids=movie['genre_ids'],
+        adult=movie['adult'],
+        original_language=movie['original_language'],
+        release_date=movie['release_date'],
+        overview=movie['overview'],
+        vote_average=movie['vote_average'],
+        popularity=movie['popularity'],
+        poster_path=movie['poster_path'],
+        genres=movie_categories
+      )
+
+      existing_movie = Movie.query.filter_by(title=movie['title']).first()
+
+      if not existing_movie:
+        db.session.add(movie_data)
+        db.session.commit()
+
+    return jsonify({'message': 'new movies added at database, try it again.'})
+
+  existing_favorite = Favorite.query.filter_by(user_id=user_id, movie_id=movie_id).first()
+
+  if existing_favorite:
+      return jsonify({'message': 'This movie is already in favorites.'}), 400
+
+  favorite_data = Favorite(
+      user_id=user_id,
+      movie_id=movie_id
+  )
+
+  created_movie_at_favorite = Movie.query.filter_by(id=favorite_data.movie_id).first()
+
+  db.session.add(favorite_data)
+  db.session.commit()
+
+  return jsonify({'message': f'{created_movie_at_favorite.id} was successfully added to favorites.'})
 
 if __name__ == '__main__':
   app.run(debug=True)
